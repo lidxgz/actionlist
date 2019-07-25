2949dacbff465964a76e649a0f8225a259fa9fac
lidxgz <lidxgz@163.com>
2019-07-25 21:31:36 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'actionlist'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
